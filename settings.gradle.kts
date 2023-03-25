a00c490b6b9cc5b23d139db6caffc5a8bdabb7f1
Khaupshev Eldar <khaupsheveldar@gmail.com>
2023-03-25 18:31:44 +0300

    [m1-init] add version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,11 @@
 rootProject.name = "202212-ok-marketplace"
 
+pluginManagement {
+    val kotlinVersion: String by settings
+
+    plugins {
+        kotlin("jvm") version kotlinVersion
+    }
+}
+
 include("m1l1-quickstart")
